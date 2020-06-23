c9d573f00be18743e1ad43a68cff508f0e3fecbb
MuhammadShahrukh <engr.shahrukh786@gmail.com>
2020-06-23 22:16:48 +0500

    updated

--- js/chapter21-25.js
+++ js/chapter21-25.js
@@ -33,40 +33,40 @@ document.write("<p>Character at Index 3 : " + (str1.charAt(3)) + "</p><br>");
 
 var firstName = prompt("Enter Your First Name");
 var lastName = prompt("Enter your Last Name");
-var fullName = firstName.concat(" " ,lastName);
+var fullName = firstName.concat(" ", lastName);
 alert(fullName);
 
 //----------------------------------------Question no 7-----------------------------------------------------
 
 var city1 = "Hyderabad";
 document.write("<p>City : " + city1 + "</p>");
-document.write("<p>After Replacement : " + (city1.replace('Hyder', 'Islam')) +"</p><br>")
+document.write("<p>After Replacement : " + (city1.replace('Hyder', 'Islam')) + "</p><br>")
 
 //----------------------------------------Question no 8-----------------------------------------------------
 
 var msg = "Ali and Sami are best friends. They play cricket and football together.";
 document.write("<p>Message : " + msg + "</p>");
-document.write("<p>After Replacement of and : " + (msg.replace(/and/g, '&')) +"</p><br>");
+document.write("<p>After Replacement of and : " + (msg.replace(/and/g, '&')) + "</p><br>");
 
 //----------------------------------------Question no 9-----------------------------------------------------
 
 var str2 = '472';
-document.write("<p>Value : " + str2 +"</p>");
-document.write("<p>Type : " + typeof(str2) +"</p>");
+document.write("<p>Value : " + str2 + "</p>");
+document.write("<p>Type : " + typeof(str2) + "</p>");
 str2 = Number(str2);
-document.write("<p>Value : " + str2 +"</p>");
-document.write("<p>Value : " + typeof(str2) +"</p><br>");
+document.write("<p>Value : " + str2 + "</p>");
+document.write("<p>Value : " + typeof(str2) + "</p><br>");
 
 //----------------------------------------Question no 10-----------------------------------------------------
 
 var input = prompt("Enter Any thing");
-document.write("<p>User Input : " + input +"</p>");
+document.write("<p>User Input : " + input + "</p>");
 document.write("<p>Upper Case : " + input.toUpperCase() + "</p><br>");
 
 //----------------------------------------Question no 11-----------------------------------------------------
 
 var input1 = prompt("Enter any thing");
-var title = input1.slice(0,1).toUpperCase() + input1.slice(1).toLowerCase();
+var title = input1.slice(0, 1).toUpperCase() + input1.slice(1).toLowerCase();
 document.write("<p>User Input : " + input1 + "</p>");
 document.write("<p>Title Case : " + title + "</p><br>");
 
@@ -79,44 +79,41 @@ document.write("<p>Result : " + ((String(num)).replace('.', '')) + "</p><br>");
 //----------------------------------------Question no 13-----------------------------------------------------
 
 var userName = prompt("Enter Your User name");
-for(var i=0; i<userName.length; i++){
+for (var i = 0; i < userName.length; i++) {
     var check = userName[i].charCodeAt(0);
-    if ((check>32 && check<=47) || (check>57 && check<=64) || (check>90 && check<=96) || (check>122 && check<=127)){
+    if ((check > 32 && check <= 47) || (check > 57 && check <= 64) || (check > 90 && check <= 96) || (check > 122 && check <= 127)) {
         alert("Enter Valid User Name");
     }
 }
 
 //----------------------------------------Question no 14-----------------------------------------------------
 
-var A = ['cake','apple pie','cookie','chips','patties'];
-var chek,j;
+var A = ['cake', 'apple pie', 'cookie', 'chips', 'patties'];
+var chek, j;
 var user = prompt("Welcome to My Bakery. What do you want to order sir/ma'am?").toLowerCase();
-for (var i=0; i<A.length;i++){
-    if (user === A[i]){
+for (var i = 0; i < A.length; i++) {
+    if (user === A[i]) {
         chek = A[i];
         j = i;
         break;
     }
 }
-if(user === chek){
-    document.write(user + " is <b>Available</b> at index " + j+ " in our Bakery");
-}
-else{
-    document.write("We are sorry. "+user+ " is <b>not available</b> in our Bakery");
+if (user === chek) {
+    document.write(user + " is <b>Available</b> at index " + j + " in our Bakery");
+} else {
+    document.write("We are sorry. " + user + " is <b>not available</b> in our Bakery");
 }
 
 //----------------------------------------Question no 15-----------------------------------------------------
 
 var pasw = prompt("Enter a Password");
-if(pasw.length >= 6){
-    if((pasw.charCodeAt(0)>=48) && (pasw.charCodeAt(0)<58)){
+if (pasw.length >= 6) {
+    if ((pasw.charCodeAt(0) >= 48) && (pasw.charCodeAt(0) < 58)) {
 
-    }
-    else{
+    } else {
         alert("Password Can not begin with a Number");
     }
-}
-else{
+} else {
     alert("Not accepted criteria isn't  met. Password length should be six or more than six");
 }
 
@@ -124,23 +121,24 @@ else{
 
 var uni = 'University of Karachi';
 var uni1 = uni.split('');
-for(var i=0; i<uni1.length; i++){
-document.write(uni1[i] +"<br>");}
+for (var i = 0; i < uni1.length; i++) {
+    document.write(uni1[i] + "<br>");
+}
 
 //----------------------------------------Question no 17-----------------------------------------------------
 
 var input = prompt("Enter Any String");
 document.write("<p>User Input : " + input + "</p>");
-document.write("<p>Last Character of Input : " + (input.charAt(input.length-1) + "</p><br>"));
+document.write("<p>Last Character of Input : " + (input.charAt(input.length - 1) + "</p><br>"));
 
 //----------------------------------------Question no 18-----------------------------------------------------
 
 var str = "The quick brown fox jumps over the lazy dog";
 var str1 = str.split(' ');
 var match = [];
-for(var i=0; i<str1.length; i++){
-    for(var j=i+1; j<str1.length; j++){
-        if(str1[i].toLowerCase()===str1[j].toLowerCase()){
+for (var i = 0; i < str1.length; i++) {
+    for (var j = i + 1; j < str1.length; j++) {
+        if (str1[i].toLowerCase() === str1[j].toLowerCase()) {
             match.push(str1[i]);
             match.push(str1[j]);
             // break;
--- js/chapter35-38.js
+++ js/chapter35-38.js
@@ -1,116 +1,189 @@
-// function date(){
-//     var date = new Date();
-//     document.write("<h2>Current Date and time is " + date +"</h2>");
-// }
-// function fullname(firstName,lastName){
-//     document.write("<h3>Well Come!</h3>");
-//     document.write("<h3>" + firstName + " " + lastName + "</h3>");
-// }
-// function add(num1,num2){
-//     return num1 + num2
-// }
-// function calculator(num1,num2,op){
-//     if(op==='+'){
-//         return num1 + num2
-//     }
-//     else if(op==='-'){
-//         if(num1 > num2){
-//             return num1 - num2
-//         }
-//         else{
-//             return num2 - num1
-//         }
-//     }
-//     else if(op==='*'){
-//         return num1 * num2
-//     }
-//     else if(op==='/'){
-//         return num1 / num2
-//     }
-//     else{
-//         alert("Enter correct operator eg (+ - * /)")
-//     }
-// }
-// function square(num){
-//     return num * num
-// }
-// function factorial(fac){
-//     var a = 1;
-//     if(fac > 0){
-//         for (var i=fac; i>0; i--){
-//             a = a*i
-//         }
-//         return a
-//     }
-//     else if(fac==0){
-//         return 1
-//     }
-//     else{
-//         return "Enter Positive Number only"
-//     }
-// }
-// date();
-// var firstName = prompt("Enter Your First Name");
-// var lastName = prompt("Enter Your Last Name");
-// fullname(firstName,lastName);
-// var num1 = Number(prompt("Enter First Number for addition"));
-// var num2 = Number(prompt("Enter Second Number for addition"));
-// var num3 = add(num1,num2);
-// document.write("<h3>Addition</h3>");
-// document.write("Addition of " +num1 + " & " + num2 +" is "+num3);
-// var num1 = Number(prompt("Calculator\nEnter First Number"));
-// var num2 = Number(prompt("Calculator\nEnter Second Number"));
-// var op = prompt("Calculator\nEnter Operator like('+', '-', '*', '/'");
-// var result = calculator(num1,num2,op);
-// document.write("<h3>Calculator</h3>");
+function date() {
+    var date = new Date();
+    document.write("<h2>Current Date and time is " + date + "</h2>");
+}
 
-// if(op==='+'){
-//     document.write("Additon of " + num1 + " & " + num2 +" is " + result);
-// }
-// else if(op==='-'){
-//     if(num1 > num2){
-//     document.write("Subtraction of " + num1 + " & " + num2 +" is " + result);}
-//     else{
-//     document.write("Subtraction of " + num2 + " & " + num1 +" is " + result);
-//     }
-// }
-// else if(op==='*'){
-//     document.write("Multiplicaton of " + num1 + " & " + num2 +" is " + result);
-// }
-// else if(op==='/'){
-//     document.write("Division of " + num1 + " & " + num2 +" is " + result);
-// }
-// var squar = Number(prompt("Enter any number which you want to find square"));
-// var ans = square(squar);
-// document.write("<h3>Square</h3>");
-// document.write("Square of " + squar + " is " + ans);
-// var fac = Number(prompt("Enter Positive for finding factorial"));
-// var a = factorial(fac);
-// document.write("<h3>Factorial</h3>");
-// document.write("Factorial of " + fac + " is  " + a);
+function fullname(firstName, lastName) {
+    document.write("<h3>Well Come!</h3>");
+    document.write("<h3>" + firstName + " " + lastName + "</h3>");
+}
 
-// function counting(start,end){
-//     for(var i=start; i<=end; i++){
-//         document.write(i + "<br>")
-//     }
-// }
-// var start = Number(prompt("Enter starting number of counting"));
-// var end = Number(prompt("Enter ending number of counting"));
-// document.write("<h3>Counting</h3>");
-// counting(start,end);
+function add(num1, num2) {
+    return num1 + num2
+}
 
-// function caclsquare(num){
-//     return num * num
-// }
-// function calculateHypotenuse(prep,base){
-//     prep = caclsquare(prep)
-//     base = caclsquare(base)
-//     var hyp = Math.pow((prep + base),(1/2))
-//     return hyp
-// }
-// var prep = Number(prompt("Enter Perpendicular of the right angle triangle"))
-// var base = Number(prompt("Enter Base of the right angle triangle"))
-// var hyp = calculateHypotenuse(prep,base)
-// document.write("<h3>Hypotenuse of Right Angle triangle</h3>");
-// document.write("Hypotenuse of given triangle is " + hyp);
+function calculator(num1, num2, op) {
+    if (op === '+') {
+        return num1 + num2
+    } else if (op === '-') {
+        if (num1 > num2) {
+            return num1 - num2
+        } else {
+            return num2 - num1
+        }
+    } else if (op === '*') {
+        return num1 * num2
+    } else if (op === '/') {
+        return num1 / num2
+    } else {
+        alert("Enter correct operator eg (+ - * /)")
+    }
+}
 
+function square(num) {
+    return num * num
+}
+
+function factorial(fac) {
+    var a = 1;
+    if (fac > 0) {
+        for (var i = fac; i > 0; i--) {
+            a = a * i
+        }
+        return a
+    } else if (fac == 0) {
+        return 1
+    } else {
+        return "Enter Positive Number only"
+    }
+}
+date();
+var firstName = prompt("Enter Your First Name");
+var lastName = prompt("Enter Your Last Name");
+fullname(firstName, lastName);
+var num1 = Number(prompt("Enter First Number for addition"));
+var num2 = Number(prompt("Enter Second Number for addition"));
+var num3 = add(num1, num2);
+document.write("<h3>Addition</h3>");
+document.write("Addition of " + num1 + " & " + num2 + " is " + num3);
+var num1 = Number(prompt("Calculator\nEnter First Number"));
+var num2 = Number(prompt("Calculator\nEnter Second Number"));
+var op = prompt("Calculator\nEnter Operator like('+', '-', '*', '/'");
+var result = calculator(num1, num2, op);
+document.write("<h3>Calculator</h3>");
+
+if (op === '+') {
+    document.write("Additon of " + num1 + " & " + num2 + " is " + result);
+} else if (op === '-') {
+    if (num1 > num2) {
+        document.write("Subtraction of " + num1 + " & " + num2 + " is " + result);
+    } else {
+        document.write("Subtraction of " + num2 + " & " + num1 + " is " + result);
+    }
+} else if (op === '*') {
+    document.write("Multiplicaton of " + num1 + " & " + num2 + " is " + result);
+} else if (op === '/') {
+    document.write("Division of " + num1 + " & " + num2 + " is " + result);
+}
+var squar = Number(prompt("Enter any number which you want to find square"));
+var ans = square(squar);
+document.write("<h3>Square</h3>");
+document.write("Square of " + squar + " is " + ans);
+var fac = Number(prompt("Enter Positive for finding factorial"));
+var a = factorial(fac);
+document.write("<h3>Factorial</h3>");
+document.write("Factorial of " + fac + " is  " + a);
+
+function counting(start, end) {
+    for (var i = start; i <= end; i++) {
+        document.write(i + "<br>")
+    }
+}
+var start = Number(prompt("Enter starting number of counting"));
+var end = Number(prompt("Enter ending number of counting"));
+document.write("<h3>Counting</h3>");
+counting(start, end);
+
+function caclsquare(num) {
+    return num * num
+}
+
+function calculateHypotenuse(prep, base) {
+    prep = caclsquare(prep)
+    base = caclsquare(base)
+    var hyp = Math.pow((prep + base), (1 / 2))
+    return hyp
+}
+var prep = Number(prompt("Enter Perpendicular of the right angle triangle"))
+var base = Number(prompt("Enter Base of the right angle triangle"))
+var hyp = calculateHypotenuse(prep, base)
+document.write("<h3>Hypotenuse of Right Angle triangle</h3>");
+document.write("Hypotenuse of given triangle is " + hyp);
+
+function area(width, height) {
+    document.write("<h3>Area of Rectangle</h3>")
+    document.write("area of rectangle is " + (width * height))
+}
+var width = Number(prompt("Enter width of the rectangle"))
+var height = Number(prompt("Enter height of the rectangle"))
+area(width, height)
+
+function palindrome(str) {
+    var str1 = str.toLowerCase().replace(/[\W_]/g, '')
+    var reverse = str1.split('').reverse().join('')
+    if (str1 === reverse) {
+        document.write("<h3>Plaindrome String</h3>")
+        document.write(str + " is Plaindrome")
+    } else {
+        document.write("<h3>Plaindrome String</h3>")
+        document.write(str + " is not a Plaindrome")
+
+    }
+}
+var str = prompt("Enter a String to check which is pliandrome or not")
+palindrome(str);
+
+function capitaliz(str) {
+    str = str.split(' ')
+    var capital = []
+    for (var i = 0; i < str.length; i++) {
+        capital.push(str[i].slice(0, 1).toUpperCase() + str[i].slice(1).toLowerCase())
+    }
+    document.write("<h3>Capitalize String</h3>")
+    document.write("String is " + str.join(' ') + "<br>")
+    document.write("String is Capitalize " + capital.join(' '))
+}
+var str = prompt("Enter String in Lower Case or Upper case as you wish")
+capitaliz(str)
+
+function longword(str) {
+    var indexArray = []
+    var str1 = str.split(' ')
+    for (var i = 0; i < str1.length; i++) {
+        indexArray.push(str1[i].length)
+    }
+    var maxIndex = indexArray.indexOf(indexArray.reduce(function(a, b) { return Math.max(a, b) }))
+    document.write("<h3>Finding Longest word in a string</h3>")
+    document.write("String is " + str + "<br>")
+    document.write("Longest word is " + str1[maxIndex])
+}
+var str = prompt("Enter String ")
+longword(str)
+
+function occurence(str, letter) {
+    var str1 = str.split('')
+    var count = 0
+    for (var i = 0; i < str1.length; i++) {
+        if (str1[i] === letter) {
+            count += 1
+        }
+    }
+    document.write("<h3>Searching Occurrences " + letter + " in " + str + "</h3>")
+    document.write(letter + " is " + count + " times repeat in " + str)
+}
+var str = prompt("Enter Any string")
+var letter = prompt("Enter a letter which you want to find number of occurrence")
+occurence(str, letter)
+
+function calcCircumference(radius) {
+    document.write("<h3>Circumference of Circle</h3>")
+    document.write("The circumference is " + (2 * Math.PI * radius))
+}
+
+function calcArea(radius) {
+    document.write("<h3>Area of Circle</h3>")
+    document.write("The Area is " + ((Math.PI) * (Math.pow(radius, 2))))
+}
+var radius = Number(prompt("Enter Radius of the circle"))
+calcCircumference(radius)
+calcArea(radius)
